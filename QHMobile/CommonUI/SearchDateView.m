92bb60731738dffcc1f2a840fd338d45a71d8d48
yao <yao@yaotekiMacBook-Pro.local>
2015-02-12 23:17:51 +0800

    日期控件

--- QHMobile/CommonUI/SearchDateView.m
+++ QHMobile/CommonUI/SearchDateView.m
@@ -8,6 +8,11 @@
 
 #import "SearchDateView.h"
 const static CGFloat labelWidth = 80;
+
+@interface SearchDateView()
+@property (nonatomic,strong) UIDatePicker           *datePicker;
+
+@end
 @implementation SearchDateView
 
 /*
@@ -26,17 +31,42 @@ const static CGFloat labelWidth = 80;
         startLab.backgroundColor = [UIColor yellowColor];
         [self addSubview:startLab];
         
-        UILabel *testLab = [[UILabel alloc] initWithFrame:CGRectMake(150, 0, labelWidth, 30)];
-        testLab.text = @"日期控件";
-        testLab.backgroundColor = [UIColor blueColor];
-        testLab.textColor = [UIColor whiteColor];
-        [self addSubview:testLab];
+//        UILabel *testLab = [[UILabel alloc] initWithFrame:CGRectMake(150, 0, labelWidth, 30)];
+//        testLab.text = @"日期控件";
+//        testLab.backgroundColor = [UIColor blueColor];
+//        testLab.textColor = [UIColor whiteColor];
+//        [self addSubview:testLab];
 
         UILabel *endLab = [[UILabel alloc] initWithFrame:CGRectMake(250, 0, labelWidth, 30)];
         endLab.text = @"结束日期";
         endLab.backgroundColor = [UIColor yellowColor];
         [self addSubview:endLab];
+      
+        _datePicker = [[UIDatePicker alloc] initWithFrame:CGRectMake(0, 0, 200, 150)];
+        _datePicker.backgroundColor = [UIColor whiteColor];
+
+        UIToolbar *tools = [[UIToolbar alloc] initWithFrame:CGRectMake(50, 0, 200, 50)];
+        UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"完成", @"") style:UIBarButtonItemStyleDone target:self action:@selector(handleActionBarDone:)];
+        UIBarButtonItem *leftflexible = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+        [tools setItems:[NSArray arrayWithObjects:leftflexible,doneButton, nil]];
+        // _datePicker.inputAccessoryView = tools;
+        //_datePicker.inputAccessoryView
+        
+        
+        UITextView *texts = [[UITextView alloc] initWithFrame:CGRectMake(100, 0, 200, 50)];
+        [self addSubview:texts];
+        // [texts becomeFirstResponder];
+        texts.backgroundColor = [UIColor yellowColor];
+        texts.inputView = _datePicker;
+        texts.inputAccessoryView = tools;
+
     }
     return self;
 }
+- (void)handleActionBarDone:(id)sender
+{
+    [self endEditing:YES];
+    
+}
+
 @end
